26089a4c475bfbb27cd27b1ab39ec709c1a4dc06
Nikolas Karadimitriou <karadimnik@gmail.com>
2022-11-05 03:29:02 +0200

    feat(socat|teamspeak): Overcome netsh UDP support limitation | Forward ports for TeamSpeak

--- ip-forward.cmd
+++ ip-forward.cmd
@@ -1,7 +1,11 @@
 @echo off
 SET runningPath=%~dp0
-echo %runningPath:~0,-1%
+echo "Current directory: %runningPath:~0,-1%"  >  "%runningPath:~0,-1%\ip-forward-cmd.log" 2>&1
 @echo on
 
-PowerShell -Command "Set-ExecutionPolicy Unrestricted" > "%runningPath:~0,-1%\ip-forward-cmd.log" 2>&1
-PowerShell "%runningPath:~0,-1%\ip-forward.ps1" >> "%runningPath:~0,-1%\ip-forward-cmd.log" 2>&1
+echo "$ Execution Policy" >>  "%runningPath:~0,-1%\ip-forward-cmd.log" 2>&1
+PowerShell -Command "Set-ExecutionPolicy ByPass" >> "%runningPath:~0,-1%\ip-forward-cmd.log" 2>&1
+echo "$ Unblock-File" >>  "%runningPath:~0,-1%\ip-forward-cmd.log" 2>&1
+PowerShell -Command "Unblock-File %runningPath:~0,-1%\ip-forward.ps1"  >> "%runningPath:~0,-1%\ip-forward-cmd.log" 2>&1
+echo "$ Run [ip-forward.ps1]" >>  "%runningPath:~0,-1%\ip-forward-cmd.log" 2>&1
+PowerShell "%runningPath:~0,-1%\ip-forward.ps1" 
--- ip-forward.ps1
+++ ip-forward.ps1
@@ -1,7 +1,7 @@
-$runningDir = Get-Location
+$runningDir = $PSScriptRoot
 $debug = 0
 Write-Host "Current Script Location: [ $runningDir ]"
-
+Add-Content -Path "$runningDir\ip-forward.log" -Value "#############################################################################################"
 function Get-CommandWithTimeStamp {
     param (
         $Command
@@ -21,8 +21,19 @@ function Get-CommandWithTimeStamp {
     }
     return $result
 } 
-Get-CommandWithTimeStamp -Command "wsl --list --running"
-Get-CommandWithTimeStamp -Command "Get-Location"
+
+# Get-CommandWithTimeStamp -Command "wsl --list --running"
+# Get-CommandWithTimeStamp -Command "Get-Location"
 $wsl_ip = (Get-CommandWithTimeStamp -Command "(wsl hostname -I).trim().split(' ')[0]")
 Get-CommandWithTimeStamp -Command "(netsh interface portproxy add v4tov4 listenport=80 connectport=80 connectaddress=$wsl_ip)"
+Get-CommandWithTimeStamp -Command "(netsh interface portproxy add v4tov4 listenport=9987 connectport=9987 connectaddress=$wsl_ip)"
+Get-CommandWithTimeStamp -Command "(netsh interface portproxy add v4tov4 listenport=10011 connectport=10011 connectaddress=$wsl_ip)"
+Get-CommandWithTimeStamp -Command "(netsh interface portproxy add v4tov4 listenport=30033 connectport=30033 connectaddress=$wsl_ip)"
 Get-CommandWithTimeStamp -Command "(netsh interface portproxy show v4tov4)"
+## Bypass netsh UDP limitation using socat
+Get-CommandWithTimeStamp -Command "(Start-Process -FilePath 'C:\socat-1.7.3.2-1-x86_64\socat.exe' -ArgumentList 'udp4-listen:9987,reuseaddr,fork tcp:localhost:9987' -WindowStyle Hidden)"
+Get-CommandWithTimeStamp -Command "(Start-Process -FilePath 'wsl' -ArgumentList 'socat tcp4-listen:9987,reuseaddr,fork UDP:localhost:9987' -WindowStyle Hidden)"
+
+Add-Content -Path "$runningDir\ip-forward.log" -Value ""
+
+
